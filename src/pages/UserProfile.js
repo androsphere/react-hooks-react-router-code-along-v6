b334fab30abf76a9377dd4ad065485421b152f91
Andrew MacIsaac <amacisaac95@gmail.com>
2023-11-14 14:09:35 -0700

    codealong complete

---
+++ src/pages/UserProfile.js
@@ -0,0 +1,36 @@
+import NavBar from "../components/NavBar";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+
+
+function UserProfile() {
+    const params = useParams();
+    const userId = params.id;
+    const [user, setUser] = useState({});
+
+    useEffect(() =>{
+        fetch(`http://localhost:4000/users/${userId}`)
+        .then(r => r.json())
+        .then(data => setUser(data))
+        .catch(error => console.error(error));
+      }, [userId]);
+
+      if(!user.name){
+        return <h1>Loading...</h1>;
+      };
+
+
+  return(
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>{user.name}</h1>
+      </main>
+    </>
+  );
+};
+
+export default UserProfile;
